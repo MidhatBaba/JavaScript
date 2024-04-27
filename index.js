669ca5e6eb606a556bdc684a2c96ea1dda4a1681
Midhat <midhatbaba424@gmail.com>
2024-04-27 20:55:41 +0530

    Code added

---
+++ index.js
@@ -0,0 +1,11 @@
+var x=0;
+function Increment(){
+    x=x+1;
+    document.getElementById("count-el").innerText=x;
+}
+function save()
+{
+    document.getElementById("enteries").textContent += x + " - ";
+    document.getElementById("count-el").textContent=0;
+    x=0;
+}
