a4672f09ca18fbf258021289ce2f019e3dae2008
Midhat <midhatbaba424@gmail.com>
2024-04-28 20:53:14 +0530

    Chnaged files

--- Black_jack/index.js
+++ Black_jack/index.js
@@ -3,9 +3,9 @@ var card_2 = 7
 let sum = card_1+ card_2
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    console.log("Do you want to draw a new card? ")
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    console.log("Wohoo! You've got Blackjack! ")
 } else {
-    console.log("You're out of the game! 😭")
+    console.log("You're out of the game! ")
 }
