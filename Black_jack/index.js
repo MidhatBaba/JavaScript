6743f9069232f410405d03f86de0a0d3282c8ece
Midhat <midhatbaba424@gmail.com>
2024-04-28 20:52:00 +0530

    Added content

---
+++ Black_jack/index.js
@@ -0,0 +1,11 @@
+var card_1 = 10
+var card_2 = 7
+let sum = card_1+ card_2
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
