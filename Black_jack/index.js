f6a0bdf5d0a3d78f916a02f86faa05896ed5fc04
Midhat <midhatbaba424@gmail.com>
2024-04-29 22:25:41 +0530

    Added changes

--- Black_jack/index.js
+++ Black_jack/index.js
@@ -1,19 +1,23 @@
 let card_1 = 10
-let card_2 = 11
-let sum = card_1+ card_2
+let card_2 = 1
+let sum = card_1 + card_2
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-function Start_game(){
+
+function startGame()
+{
 if (sum <= 20) {
     message= "Do you want to draw a new card?"
-} else if (sum === 21) {
+} 
+else if (sum === 21) 
+{
     message ="Wohoo! You've got Blackjack! "
     hasBlackJack = true
 } else {
     message="You're out of the game! "
     isAlive = false
 }
-console.log(message)
+document.getElementById("message-el").textContent= message
 }
