f88e240e03bd031587717714e0fdf1f178f26781
Midhat <midhatbaba424@gmail.com>
2024-04-30 12:57:13 +0530

    changed index

--- Black_jack/index.js
+++ Black_jack/index.js
@@ -1,36 +1,43 @@
-let card_1 = 10
-let card_2 = 11
+let card_1 =57
+let card_2 =
+let cards = [firstCard, secondCard]
 let sum = card_1 + card_2
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+function startGame()
+{
+    renderGame()
+}
+
 function newCard()
 {
-
+    let n_card=4
+     sum += n_card
+    cards.push(cards)
+    console.log(cards)
+     renderGame()
 }
 
 
-function startGame()
+function renderGame()
 {
+    document.querySelector(".card-el").textContent= "Cards :"
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i]
+    }
 if (sum <= 20) {
     message= "Do you want to draw a new card?"
 } 
 else if (sum === 21) 
 {
-    message ="Wohoo! You've got Blackjack! "
+    message ="You've got Blackjack! "
     hasBlackJack = true
 } else {
     message="You're out of the game! "
     isAlive = false
 }
 document.getElementById("message-el").textContent= message
-// document.getElementById("sum-el").textContent= "Sum :" +" " + sum to retrieve an id, use getElementById
-//mainly used for retreiving a class or a tag
 document.querySelector(".sum-el").textContent= "Sum :" +" " + sum
-document.querySelector(".card-el").textContent= "Cards :" + card_1 +" " +card_2
-
-
-
-
 }
